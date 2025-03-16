cebd41c1a484d9bbd07903e487a3a93fe637360c
AnandUmak <anandumak1999@gmail.com>
2025-03-16 20:57:39 +0530

    first commit

---
+++ Chinmay1.js
@@ -0,0 +1,55 @@
+// method of array by Chinmay
+
+//Q1 - Find age and make new array of age from YOB
+
+// let y = [1995,1990,1985,1980]
+
+// let age = []
+
+// for(let i = 0;i< y.length;i++){
+//     age.push(2024 - y[i])
+// }
+// console.log(age)
+
+//--------------------------------------------------------
+
+//Q2 - make array of larger no than 35 from array of no
+
+// let m = [30,33,35,60,70,80]
+
+// let great = []
+
+// for(let i = 0;i<m.length;i++){
+//     if(m[i]>35){
+//         great.push(m[i])
+//     }
+// }
+// console.log(great)
+
+//---------------------------------------------------------
+
+//Q3 - cummulative sum of array
+
+// let s = [10,20,30,40]
+
+// let sum = 0
+
+// for(let i = 0;i< s.length;i++){
+//     sum = sum + s[i]
+// }
+// console.log(sum)
+
+//-----------------------------------------------------------
+
+//Q4 - print every elememnt of array
+
+let name = ["Pune","Mumbai","Chennai","Nagpur"]
+
+for(let i = 0;i< name.length;i++){
+    console.log("Welcome to "+ name[i])
+}
+
+
+
+
+
